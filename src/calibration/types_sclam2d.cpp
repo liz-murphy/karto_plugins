87c2e12ff743168e3a512f04dd29072e633875f6
liz-murphy <liz.murphy80@gmail.com>
2014-09-03 15:30:00 -0700

    Added calibration

---
+++ src/calibration/types_sclam2d.cpp
@@ -0,0 +1,12 @@
+#include <karto_plugins/calibration/types_sclam2d.h>
+
+#include "g2o/core/factory.h"
+#include "g2o/stuff/macros.h"
+
+#include <iostream>
+
+namespace g2o {
+  G2O_REGISTER_TYPE_GROUP(calibration);
+  G2O_REGISTER_TYPE(ROBOT_LASER_SCLAM, RobotLaserSCLAM);
+
+} // end namespace
